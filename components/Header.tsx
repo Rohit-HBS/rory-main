3cdcefdae9fd31d4f16f380ccbacd5beec66ba4c
Rohit-HBS <you@example.com>
2025-01-06 17:44:46 +0530

    theme change

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,39 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import NavLink from './NavLink';
-import {Sun, Moon} from 'react-feather';
+import { Sun, Moon } from 'react-feather';
 
-interface HeaderProps{
-    mounted: any,
-    resolvedTheme: any,
-    setTheme: any,
-    scrolled: any
+interface HeaderProps {
+    mounted: boolean;
+    resolvedTheme: string;
+    setTheme: (theme: string) => void;
+    scrolled: boolean;
 }
-const Header = ({mounted, resolvedTheme, setTheme, scrolled}: HeaderProps): JSX.Element => {
+
+const Header = ({ mounted, resolvedTheme, setTheme, scrolled }: HeaderProps): JSX.Element => {
     return (
-        <header className={`z-10 pt-10 pb-1 mb-10 pl-0 top-0 right-0 left-0 transition border-b ${scrolled ? 'border-gray-400' : 'bg-transparent border-transparent'} sticky w-screen backdrop-filter backdrop-blur-md`}>
+        <header
+            className={`z-10 pt-10 pb-1 mb-10 pl-0 top-0 right-0 left-0 transition border-b ${
+                scrolled ? 'border-gray-400' : 'bg-transparent border-transparent'
+            } sticky w-screen backdrop-filter backdrop-blur-md`}
+        >
             <div className="h-0 pb-9 pl-5 max-w-4xl w-full flex items-center justify-between m-auto">
                 <Link href="/">
                     <div>
-                        <Image className="cursor-pointer transition-colors flex !p-1 rounded-md mr-8 hover:bg-gray-300 text-lg dark:filter dark:invert"
+                        <Image
+                            className="cursor-pointer transition-colors flex !p-1 rounded-md mr-8 hover:bg-gray-300 text-lg dark:filter dark:invert"
                             src="/favicon/favicon.svg"
                             width={45}
                             height={45}
-                            alt="favicon"/>
+                            alt="favicon"
+                        />
                     </div>
                 </Link>
                 <nav className="flex items-center justify-between pr-4">
-                    <NavLink title="About" href="/#about"/>
-                    <NavLink title="Publications" href="/#publications"/>
-                    <NavLink title="Projects" href="/projects"/>
-                    <NavLink title="Misc" href="/misc"/>
+                    <NavLink title="About" href="/#about" />
+                    <NavLink title="Publications" href="/#publications" />
+                    <NavLink title="Projects" href="/projects" />
+                    <NavLink title="Misc" href="/misc" />
 
                     <button
                         type="button"
@@ -35,28 +42,20 @@ const Header = ({mounted, resolvedTheme, setTheme, scrolled}: HeaderProps): JSX.
                         disabled={!mounted}
                         onClick={() => {
                             setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
-                        }}>
+                        }}
+                    >
                         <abbr title="Switch theme">
                             {resolvedTheme === 'light' ? (
-                                <Moon
-                                    size={22}
-                                    aria-label="Moon"
-                                />
+                                <Moon size={22} aria-label="Moon" />
                             ) : (
-                                <Sun
-                                    size={22}
-                                    aria-label="Sun"
-                                />
+                                <Sun size={22} aria-label="Sun" />
                             )}
-                        </abbr> 
-                    </button> 
+                        </abbr>
+                    </button>
                 </nav>
             </div>
         </header>
-        
     );
 };
-
-
 
 export default Header;
